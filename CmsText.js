9f5ccde0132485933626fefd02c4116af7398eb9
rajibnagpersonal-rgb <rajibnagpersonal@gmail.com>
2026-08-24 21:44:01 +0530

    Delete CmsText.js

--- CmsText.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const cmsTextSchema = new mongoose.Schema({
-  pageKey: { type: String, required: true, unique: true }, // e.g. "global_text", "catalog_page"
-  content: { type: Object, default: {} }
-}, { timestamps: true });
-
-module.exports = mongoose.model('CmsText', cmsTextSchema);
