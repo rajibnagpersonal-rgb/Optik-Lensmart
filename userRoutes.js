e6675daed500d1f2238c7736101fb0b05a23bf23
rajibnagpersonal-rgb <rajibnagpersonal@gmail.com>
2026-08-24 21:49:59 +0530

    Delete userRoutes.js

--- userRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { syncUserProfile, getUserProfile } = require('../controllers/userController');
-
-router.post('/sync', syncUserProfile);
-router.get('/profile', getUserProfile);
-
-module.exports = router;
