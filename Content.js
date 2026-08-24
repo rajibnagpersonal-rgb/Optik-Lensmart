bbe6c94768903c492c35e2d1029553f8dc25d552
rajibnagpersonal-rgb <rajibnagpersonal@gmail.com>
2026-08-24 21:45:35 +0530

    Delete Content.js

--- Content.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const contentSchema = new mongoose.Schema({
-  page: { type: String, required: true, unique: true }, // 'home', 'terms', 'support'
-  data: { type: mongoose.Schema.Types.Mixed, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Content', contentSchema);
