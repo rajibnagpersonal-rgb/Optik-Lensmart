2c53c64e081bb357c046189ad3fb8b1c63a75f64
rajibnagpersonal-rgb <rajibnagpersonal@gmail.com>
2026-08-24 21:46:51 +0530

    Delete contentRoutes.js

--- contentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPageContent, updatePageContent } = require('../controllers/contentController');
-
-router.get('/:page', getPageContent);
-router.post('/:page', updatePageContent);
-
-module.exports = router;
