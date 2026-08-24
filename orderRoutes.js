27a91ee1e8a6e44d2aecc510d2334722fbe200f3
rajibnagpersonal-rgb <rajibnagpersonal@gmail.com>
2026-08-24 21:48:05 +0530

    Delete orderRoutes.js

--- orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { placeOrder, getMyOrders, updateOrderStage } = require('../controllers/orderController');
-
-router.post('/', placeOrder);
-router.get('/my-orders', getMyOrders);
-router.put('/:id/stage', updateOrderStage);
-
-module.exports = router;
